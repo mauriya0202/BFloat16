2a05e10f35bb55b4fdfa692cf4bff883e04c1787
Mauriya <112739882+mauriya0202@users.noreply.github.com>
2023-09-24 18:26:03 +0530

    Update parameterization.sv

--- parameterization/mac1/parameterization.sv
+++ parameterization/mac1/parameterization.sv
@@ -20,16 +20,17 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module parameterization #(parameter N=4)(a1,b1,clk1,out1);
+module parameterization #(parameter N=2)(a1,b1,cntl1,clk1,out1);
 input bit[16*N-1:0]a1,b1;
 input bit  clk1;
+input bit  cntl1;
 output bit [16*N-1:0]out1;
 genvar i;
 
 generate
 for(i=1;i<N+1;i++)
 begin
-bfloat_mac2 bf(a1[i*16-1:i*16-16],b1[i*16-1:i*16-16],clk1,out1[i*16-1:i*16-16]);
+bfloat_mac2 bf(a1[i*16-1:i*16-16],b1[i*16-1:i*16-16],cntl,clk1,out1[i*16-1:i*16-16]);
 end
 endgenerate
 
